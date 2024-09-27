1809663a897270256b00e46e633dd08616ebb68e
Sandeep Saini <sandeep.saini@evontech.com>
2024-09-27 12:33:45 +0530

    changed java version

--- LSK21AndroidShadeSelection/build.gradle.kts
+++ LSK21AndroidShadeSelection/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    //id("maven-publish")
 }
 
 android {
@@ -24,8 +25,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "1.8"
